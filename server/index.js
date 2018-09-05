5b8dbaaf86423870c7b5e6744fbd6d0e147319ea
HongyuJiang <1230223112@stu.cjlu.edu.cn>
2018-09-05 14:41:51 +0800

    deletion

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const app = express();
-
-const url = "mongodb://localhost:27017/";
 
 app.get('/regions', function (req, res) {
 
